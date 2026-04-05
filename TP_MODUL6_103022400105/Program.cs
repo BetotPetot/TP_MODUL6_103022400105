c68cd0660048654221e358eca5d5fa991c9140eb
BetotPetot <irhamfarel37@gmail.com>
2026-04-05 16:52:03 +0700

    menambahkan class SayaMusicTrack

--- TP_MODUL6_103022400105/Program.cs
+++ TP_MODUL6_103022400105/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat instance dari kelas SayaMusicTrack
+        SayaMusicTrack track = new SayaMusicTrack("Ambatukam Termuwani");
+
+        // Meningkatkan play count 
+        track.IncreasePlayCount(99);
+        // Menampilkan detail track
+        track.PrintTrackDetails();
+    }
+}
---
+++ TP_MODUL6_103022400105/SayaMusicTrack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    // Constructor
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 100000); 
+
+        this.playCount = 0;
+    }
+
+    // Method untuk menambah jumlah play count
+    public void IncreasePlayCount(int count)
+    {
+        // Untuk menambah jumlah
+        playCount += count;
+    }
+
+    // Method untuk menampilkan detail track
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+    }
+}
