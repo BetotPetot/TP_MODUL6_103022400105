fe7fe4313d2d4c4b8fb55491977090e5f68c5ec2
BetotPetot <irhamfarel37@gmail.com>
2026-04-05 17:05:20 +0700

    menambahkan implementasi design by contract

--- TP_MODUL6_103022400105/Program.cs
+++ TP_MODUL6_103022400105/Program.cs
@@ -7,9 +7,20 @@ class Program
         // Membuat instance dari kelas SayaMusicTrack
         SayaMusicTrack track = new SayaMusicTrack("Ambatukam Termuwani");
 
+
+        // NORMAL
         // Meningkatkan play count 
         track.IncreasePlayCount(99);
         // Menampilkan detail track
         track.PrintTrackDetails();
+
+        Console.WriteLine("=== TEST ERROR ===");
+
+        // Test overflow
+        for (int i = 0; i < 50; i++)
+        {
+            // Meningkatkan play count dengan nilai yang sangat besar untuk menguji overflow
+            track.IncreasePlayCount(int.MaxValue);
+        }
     }
 }
--- TP_MODUL6_103022400105/SayaMusicTrack.cs
+++ TP_MODUL6_103022400105/SayaMusicTrack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 public class SayaMusicTrack
 {
@@ -9,6 +10,11 @@ public class SayaMusicTrack
     // Constructor
     public SayaMusicTrack(string title)
     {
+        // Precondition
+        // Title tidak boleh null dan panjangnya maksimal 100 karakter
+        Debug.Assert(title != null, "Title tidak boleh null");
+        Debug.Assert(title.Length <= 100, "Title maksimal 100 karakter");
+
         this.title = title;
 
         Random rand = new Random();
@@ -20,8 +26,23 @@ public class SayaMusicTrack
     // Method untuk menambah jumlah play count
     public void IncreasePlayCount(int count)
     {
-        // Untuk menambah jumlah
-        playCount += count;
+        // Precondition
+        // Count harus lebih dari 0 dan tidak boleh melebihi 10.000.000
+        Debug.Assert(count > 0, "Count harus lebih dari 0");
+        Debug.Assert(count <= 10000000, "Maksimal 10.000.000");
+
+        // Menggunakan checked untuk menangani kemungkinan overflow pada playCount
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Terjadi overflow pada playCount");
+        }   
     }
 
     // Method untuk menampilkan detail track
